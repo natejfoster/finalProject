fe2a04e8ba89609878ce0a94f27cfa7c6a2d3274
natejfoster <nathan.96@comcast.net>
2016-12-01 13:32:33 -0800

    Added fancy menu that collapses on mobile

--- src/App.js
+++ src/App.js
@@ -2,23 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './css/App.css';
 import { Link } from 'react-router';
-import {Navbar, NavItem} from 'react-materialize';
+import $ from 'jquery';
 import '../node_modules/font-awesome/css/font-awesome.css'
+import {NavItem, Navbar} from 'react-materialize';
 
 // Needed for onTouchTap (to avoid warning from material-ui)
 // See: https://github.com/callemall/material-ui/issues/4670
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
-
 var App = React.createClass({
   render() {
     return (
       <div className="app">
-        <div className="nav">
+        <div>
+          <Navbar brand='logo' right>
             <Link className="link" activeClassName='active' to="/"><i className="fa fa-home"></i></Link>
-            <Link className="link" activeClassName='active' to="/about">About</Link>
-            <Link className="link" activeClassName='active' to="/demo">Demo</Link>
+            <NavItem><Link className="link" activeClassName='active' to="/about">About</Link></NavItem>
+            <NavItem><Link className="link" activeClassName='active' to="/demo">Demo</Link></NavItem>
+          </Navbar>
         </div>
         {this.props.children}
       </div>
