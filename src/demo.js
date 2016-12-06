7b1ee71d1d8212a11cf956af93659b593d0f489a
Govind Pillai <govindg@uw.edu>
2016-12-06 08:40:01 -0800

    Made twitter search request and show texts of tweets that are returned

--- src/demo.js
+++ src/demo.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import './css/demo.css';
+import $ from 'jquery';
 
+var baseSearchURL = 'https://twitter.com/search'
 var Demo = React.createClass({
-  render() {
-    return (
-      <div className='demo'>
-        <p>completely new filler text</p>
-      </div>
-    )
-  }
+	getInitialState() {
+		return {data:[]}
+	},
+
+	componentDidMount() {
+		$.get("https://faculty.washington.edu/joelross/proxy/twitter/search/?q=hillary%20clinton%20%3A%29&count=200", function(data) {
+			this.setState({data:data.statuses})
+			console.log(this.state.data);
+		}.bind(this));
+	},
+
+ 	render() {
+		return (
+			<div className='demo'>
+				{
+					this.state.data.map(function(d) {
+						return <p>{d.text}</p>
+					})
+				}
+			</div>
+		)
+	}
 })
 
 export default Demo;
