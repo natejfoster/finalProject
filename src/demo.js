ce54c9eb9a5041ac576c746775f9825acd79110c
natejfoster <nathan.96@comcast.net>
2016-12-06 14:17:52 -0800

    fixed ordering on small screens for comparisons

--- src/demo.js
+++ src/demo.js
@@ -11,37 +11,38 @@ var Demo = React.createClass({
 	componentDidMount() {
 		$.get("https://faculty.washington.edu/joelross/proxy/twitter/search/?q=%23hillaryclinton&result_type=popular&lang=en&count=200", function(data1) {
 			this.setState({data1:data1.statuses});
-			console.log(data1);
 		}.bind(this));
 
 		$.get("https://faculty.washington.edu/joelross/proxy/twitter/search/?q=%23donaldtrump&result_type=popular&lang=en&count=200", function(data2) {
 			this.setState({data2:data2.statuses});
-			console.log(data2);
 		}.bind(this));
 	},
 
+  formatData() {
+    var formatted = [];
+    var min = Math.min(this.state.data1.length, this.state.data2.length);
+    for (var i = 0; i < min; i++) {
+      formatted.push({text: this.state.data1[i].text, id:'data1'});
+      formatted.push({text: this.state.data2[i].text, id:'data2'});
+    }
+    return formatted;
+  },
+
  	render() {
     var min = Math.min(this.state.data1.length, this.state.data2.length);
+    var current = this.formatData();
+    console.log(current);
     return (
 			<div className='demo row container'>
-        <div className='col s12 m6' id='data1'>
-          {
-            this.state.data1.map(function(d, i) {
-              if (i < min) {
-                return <p key={'p1' + i} className='z-depth-1'>{d.text}</p>
-              }
-            })
-          }
-        </div>
-        <div className='col s12 m6' id='data2'>
-          {
-            this.state.data2.map(function(d, i) {
-              if (i < min) {
-                return <p key={'p2' + i} className='z-depth-1'>{d.text}</p>
-              }
-            })
-          }
-        </div>
+        {
+          current.map(function(d, i) {
+            return (
+              <div className='col s12 m6' id={d.id}>
+                <p key={'p1' + i} className="z-depth-1">{d.text}</p>
+              </div>
+            )
+          })
+        }
 			</div>
 		)
 	}
