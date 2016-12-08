b0733ce84dd02f7c1d9f86cdbb135b280135f879
Sam Tompkins <samtompk@gmail.com>
2016-12-08 01:01:54 -0800

    improved css on homepage

--- src/homepage.js
+++ src/homepage.js
@@ -11,8 +11,16 @@ var HomePage = React.createClass({
 				<h2>A site with a purpose that is super cool</h2>
 	    	</div>
 	    	<div id='quote-section'>
-	    		<p>"People are so gullable today. 
-	    		They might even believe that this is a real quote."
+	    		<p className='center-align'>"This is not a real quote, but a real quote from an an 
+	    		article should go here"
+	    		</p>
+	    	</div>
+	    	<div id='paragraph-section'>
+	    		<p>
+	    			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id mi pellentesque, 
+	    			dignissim nibh sed, accumsan erat. Fusce convallis mauris sit amet mauris 
+	    			sollicitudin hendrerit. Cras nec aliquam tellus. Morbi ac malesuada urna, 
+	    			at malesuada sem. Sed tortor risus, auctor sed finibus et, feugiat id lectus.
 	    		</p>
 	    	</div>
 	    </div>
