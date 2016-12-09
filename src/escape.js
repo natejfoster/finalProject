628c7bb23306049a37d4536f60b1322f4e27e648
natejfoster <nathan.96@comcast.net>
2016-12-08 17:17:29 -0800

    attempted adding embeded tweets

--- src/escape.js
+++ src/escape.js
@@ -17,9 +17,10 @@ var Escape = React.createClass({
     var formatted = [];
     var min = Math.min(this.state.data1.length, this.state.data2.length);
     for (var i = 0; i < min; i++) {
-      formatted.push({text: this.state.data1[i].text, id:'data1'});
-      formatted.push({text: this.state.data2[i].text, id:'data2'});
+      formatted.push({text: this.state.data1[i].text, id:'data1', tweet: this.state.data1[i].id.toString()});
+      formatted.push({text: this.state.data2[i].text, id:'data2', tweet: this.state.data2[i].id.toString()});
     }
+    console.log(formatted);
     return formatted;
   },
 
@@ -38,11 +39,9 @@ var Escape = React.createClass({
     $.get('https://faculty.washington.edu/joelross/proxy/twitter/search/?q=%23' + this.state.search + '%20%3A%28%20exclude%3Aretweets%20exclude%3Areplies&lang=en&count=200', function(data2) {
      this.setState({data2:data2.statuses});
     }.bind(this));
-    console.log(this.state.data1);
   },
 
  	render() {
-
     var current = this.formatData();
     return (
       <div className='escape'>
@@ -50,14 +49,11 @@ var Escape = React.createClass({
         <div className='escape row container'>
           {
             current.map(function(d, i) {
-                if (d.in_reply_to_user_id) {
-                  console.log('hello');
-                }
-                return (
-                  <div className='col s12 m6' id={d.id}>
-                    <p key={'p1' + i} className="z-depth-1 tweet">{d.text}</p>
-                  </div>
-                )
+              return (
+                <div className='col s12 m6' key={'div' + d.tweet} id={d.id}>
+                  <p key={'p1' + i} className="z-depth-1 tweet">{d.text}</p>
+                </div>
+              )
             })
           }
         </div>
